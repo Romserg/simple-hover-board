f5b1416ed899e52223c18793da89bfe6453d8d4d
Romserg <serg.romankov@gmail.com>
2021-10-11 02:17:28 +0300

    Implement border hover effect

---
+++ index.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board');
+const SQUARES_NUMBER = 506;
+const colors = ['#e74c3c', '#a54ccb', '#469dd7', '#49ff68', '#ffe459'];
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseleave', () => resetColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function resetColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #000';
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
